990bd8dab009f0d96d7bb576d7b174388bc145e7
S. Jansen <sa.jansen1@gmail.com>
2023-12-10 08:30:13 +0200

    day 1 part 2

--- src/day01.ts
+++ src/day01.ts
@@ -9,4 +9,36 @@ const part1 = async () => {
         .reduce((previousValue, currentValue) => previousValue + currentValue)
 }
 
+const part2 = async () => {
+    const numberMap = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    ]
+    const replaceWordedNumbers = (text: string): string => {
+        let result = text;
+        numberMap.forEach((placeholder, value) => {
+            if (placeholder.length === 0) return;
+            result = result.replaceAll(RegExp(`(${placeholder})`, "gi"), "$1" + value.toString() + "$1")
+        })
+        return result;
+    };
+
+    const input = await readInput("day01.txt");
+    return replaceWordedNumbers(input)
+        .replaceAll(/[a-z]/gi, '')
+        .trim()
+        .split("\n")
+        .map(it => +(it[0] + it[it.length - 1]))
+        .reduce((previousValue, currentValue) => previousValue + currentValue)
+}
+
 console.log(await part1());
+console.log(await part2());
