ed287225ac45548f0b127b4bfc200032472b5e28
S. Jansen <sa.jansen1@gmail.com>
2023-12-10 15:57:40 +0200

    day 4 part 2

--- src/day04.ts
+++ src/day04.ts
@@ -17,9 +17,33 @@ const part1 = async (): Promise<string | number> => {
         .reduce((prev, current) => prev + current, 0)
 }
 
-const part2 = async (): Promise<string | number> => {
-    const input = await readInput("day0402_test.txt");
-    return "";
+const part2 = async (): Promise<any> => {
+    const input = await readInput("day04.txt");
+    const cardMatches = input.split("\n")
+        .map(it => it.split(":")[1].trim())
+        .map(card => {
+            const [winningString, actualString] = card.split(" | ")
+            const winning = winningString.split(" ").filter(it => it.length > 0)
+            const actual = actualString.split(" ").filter(it => it.length > 0)
+
+            const matches = winning.filter(it => actual.includes(it))
+
+            return matches.length;
+        })
+
+    function getSubCardsFromCardIndex(index: number): number {
+        let result = 1;
+        for (let j = 0; j < cardMatches[index]; j++) {
+            result += getSubCardsFromCardIndex(index + j + 1);
+        }
+        return result
+    }
+
+    let result = 0;
+    for (let i = 0; i < cardMatches.length; i++) {
+        result += getSubCardsFromCardIndex(i);
+    }
+    return result
 }
 
 console.log(await part1());
