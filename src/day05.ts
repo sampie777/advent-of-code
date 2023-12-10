ab2b93eb411b4bbfa35af5c12eaa85b2bcda7686
S. Jansen <sa.jansen1@gmail.com>
2023-12-10 16:27:21 +0200

    day 5 part 1

--- src/day05.ts
+++ src/day05.ts
@@ -1,8 +1,82 @@
 import {readInput} from "./utils.ts";
 
+type SeedMap = {
+    seed: number,
+    soil: number,
+    fertilizer: number,
+    water: number,
+    light: number,
+    temperature: number,
+    humidity: number,
+    location: number
+}
+
+const getValueFromMap = (input: string, map: string, sourceID) => {
+    let mapStarted = false;
+    for (const line of input.split("\n")) {
+        if (line.includes(map)) {
+            mapStarted = true;
+            continue;
+        }
+        if (line.length == 0) mapStarted = false;
+        if (!mapStarted) continue;
+
+        const splitString = line
+            .split(" ")
+            .filter(it => it.length > 0);
+        const destinationStart = +splitString[0]
+        const seedStart = +splitString[1]
+        const length = +splitString[2]
+
+        if (sourceID < seedStart || sourceID >= seedStart + length) continue;
+
+        return destinationStart + (sourceID - seedStart);
+    }
+    return sourceID;
+};
+
+const getSeedMapForSeed = (input: string, seedID): SeedMap => {
+    const result = {
+        seed: seedID,
+        soil: 0,
+        fertilizer: 0,
+        water: 0,
+        light: 0,
+        temperature: 0,
+        humidity: 0,
+        location: 0
+    }
+
+    result.soil = getValueFromMap(input, "seed-to-soil", result.seed)
+    result.fertilizer = getValueFromMap(input, "soil-to-fertilizer", result.soil)
+    result.water = getValueFromMap(input, "fertilizer-to-water", result.fertilizer)
+    result.light = getValueFromMap(input, "water-to-light", result.water)
+    result.temperature = getValueFromMap(input, "light-to-temperature", result.light)
+    result.humidity = getValueFromMap(input, "temperature-to-humidity", result.temperature)
+    result.location = getValueFromMap(input, "humidity-to-location", result.humidity)
+
+    return result;
+}
+
 const part1 = async (): Promise<string | number> => {
-    const input = await readInput("day0501_test.txt");
-    return "";
+    const input = await readInput("day05.txt");
+
+    const seeds = input
+        .split("\n")[0]
+        .split(":")[1]
+        .split(" ")
+        .filter(it => it.length > 0)
+        .map(it => +it)
+
+    const seedMaps = seeds.map(it => getSeedMapForSeed(input, it))
+
+    let nearestLocationSeedMap = seedMaps[0];
+    seedMaps.forEach(it => {
+        if (it.location < nearestLocationSeedMap.location) {
+            nearestLocationSeedMap = it
+        }
+    })
+    return nearestLocationSeedMap.location;
 }
 
 const part2 = async (): Promise<string | number> => {
