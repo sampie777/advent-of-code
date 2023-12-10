13ae265e414d12c44b3e81542cf964bcb49f4ac2
S. Jansen <sa.jansen1@gmail.com>
2023-12-10 12:24:26 +0200

    day 3 part 2

--- src/day03.ts
+++ src/day03.ts
@@ -17,6 +17,16 @@ const checkIfSymbolAround = (lines: string[], row: number, col: number): boolean
     return false;
 };
 
+const checkIfGearAround = (lines: string[], row: number, col: number): { row: number, col: number } | null => {
+    for (let i = Math.max(0, row - 1); i < Math.min(lines.length, row + 2); i++) {
+        for (let j = Math.max(0, col - 1); j < Math.min(lines[i].length, col + 2); j++) {
+            if (lines[i][j].match(/\*/)) return {row: i, col: j};
+        }
+    }
+
+    return null;
+};
+
 const part1 = async (): Promise<string | number | number[]> => {
     const input = await readInput("day03.txt");
     const lines = input.split("\n");
@@ -57,8 +67,54 @@ const part1 = async (): Promise<string | number | number[]> => {
 }
 
 const part2 = async (): Promise<string | number> => {
-    // const input = await readInput("day0302_test.txt");
-    return "";
+    const input = await readInput("day03.txt");
+    const lines = input.split("\n");
+
+    let currentNumberString = "";
+    let nearestGearPositions: { row: number, col: number }[] = [];
+    const foundGears: { [key: number]: number[] } = {};
+
+    function finishNumber() {
+        nearestGearPositions.forEach(it => {
+            const index = it.row * lines[0].length + it.col;
+            if (!Object.keys(foundGears).includes(index.toString())) {
+                foundGears[index] = [];
+            }
+            foundGears[index].push(+currentNumberString);
+        })
+
+        currentNumberString = "";
+        nearestGearPositions = [];
+    }
+
+    for (let i = 0; i < lines.length; i++) {
+        finishNumber();
+
+        for (let j = 0; j < lines[i].length; j++) {
+            const char = lines[i][j];
+
+            if (!isNumber(char)) {
+                finishNumber();
+                continue;
+            }
+
+            const nearestGearPosition = checkIfGearAround(lines, i, j);
+            if (nearestGearPosition != null
+                && !nearestGearPositions.find(it =>
+                    it.row == nearestGearPosition!.row &&
+                    it.col == nearestGearPosition!.col)
+            ) {
+                nearestGearPositions.push(nearestGearPosition!);
+            }
+
+            currentNumberString += char;
+        }
+    }
+
+    return Object.values(foundGears)
+        .filter(it => it.length == 2)
+        .map(it => it.reduce((prev, current) => prev * current, 1))
+        .reduce((prev, current) => prev + current, 0)
 }
 
 console.log(await part1());
